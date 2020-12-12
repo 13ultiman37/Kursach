d21b50d9567883adf7b364c8cf64964208100aaf
Александр <xperiaz1981@gmail.com>
2020-12-12 16:44:43 +0400

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.header_menu').click(function(event) {
+        $('.header_menu,.menu').toggleClass('active');
+        $('.body').toggleClass('lock');
+    });
+    $('.js-select2').select2({
+    		placeholder: "Выберите город",
+    		maximumSelectionLength: 2,
+    		language: "ru"
+    	});
+    $("#selectId").change(function(){
+        $("#text").text($("#selectId option:selected").text());
+        });
+    $("#selectId").change(function(){
+        $("#text1").text($("#selectId option:selected").text());
+        });
+});
+
